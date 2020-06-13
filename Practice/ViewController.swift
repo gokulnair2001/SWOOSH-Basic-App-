b2bbc76549019fff90fa8a2d2f21a40f5058dbad
“gokulnair2001” <gokulnair.2001@gmail.com>
2020-06-13 11:58:12 +0530

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Gokul Nair on 13/06/20.
+//  Copyright © 2020 Gokul Nair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
